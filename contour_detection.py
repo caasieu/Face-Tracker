221a9859b8ef30d18f0a10d4e436880b8f11b412
Caasi2 <caasim353@gmail.com>
2024-02-25 19:34:47 -0300

    Finished for now

--- contour_detection.py
+++ contour_detection.py
@@ -2,19 +2,20 @@ import cv2
 import numpy as np 
 
 #img = np.zeros((200, 200), dtype=np.uint8)
-#img[50:150, 50:150] = 255
+#img[50:150, 50:150] = 155
+#img[100:200, 100:200] = 200
 
-img = cv2.imread('images/2.jpeg', 0)
+img = cv2.imread('images/4.jpg', 0)
 
 
-ret, thresh = cv2.threshold(img, 127, 255, 0)
+ret, thresh = cv2.threshold(img, 127, 215, 0)
 contours, hierarchy = cv2.findContours(
-	thresh, cv2.RETR_TREE, 
+	thresh, cv2.RETR_EXTERNAL, # cv2.RETR_TREE | cv2.RETR_EXTERNAL 
 	cv2.CHAIN_APPROX_SIMPLE
 )
 
 color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-img = cv2.drawContours(color, contours, -1, (0, 255, 0), 2)
+img = cv2.drawContours(color, contours, -1, (0, 255, 0), 1)
 
 
 cv2.namedWindow('Contour', cv2.WINDOW_NORMAL)
